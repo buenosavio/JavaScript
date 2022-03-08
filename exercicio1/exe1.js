40f5507a9af1ac90f7deeff316925915675c0708
Savio B <souzasavio@outlook.com>
2022-03-08 12:56:43 -0300

    fix: ajustado bugs

--- exercicio1/exe1.js
+++ exercicio1/exe1.js
@@ -3,23 +3,23 @@
     - Se o texto for igual à 'Não' solicite novamente para o usuário informar algum texto e após isso mostre para ele o texto digitado em um alerta;
     - Se for digitado qualquer outra string solicite uma confirmação para o usuário com o texto: 'Você tem noções dos seus atos?'*/
 
-let answer = prompt("Digite um texto: ")
+// let answer = prompt("Digite um texto: ")
 
-if (answer=="Sim") {
-    alert("Parabéns")
-} else if (answer=="Não") {
-    alert("Informe o texto novamente")
-    alert(`texto informado: "${answer}"`)
-} else {
-    alert("Você tem noções dos seus atos?")
-}
+// if (answer=="Sim") {
+//     alert("Parabéns")
+// } else if (answer=="Não") {
+//     alert("Informe o texto novamente")
+//     alert(`texto informado: "${answer}"`)
+// } else {
+//     confirm("Você tem noções dos seus atos?")
+// }
 
 /*2) Crie um programa que solicita para o usuário dois valores de texto, nome do cliente e nome do atendente;
 Exiba um alerta para o usuário com a mensagem "Olá cliente eu me chamo atendente, emque posso ajudar?" (String interpolation)*/
 
-let cliente = prompt("Informe nome do cliente")
-let atendente = prompt("Informe nome do atendente")
-alert(`Olá ${cliente} eu me chamo ${atendente}, em que posso ajudar?`)
+// let cliente = prompt("Informe nome do cliente")
+// let atendente = prompt("Informe nome do atendente")
+// alert(`Olá ${cliente} eu me chamo ${atendente}, em que posso ajudar?`)
 
 
 
@@ -30,20 +30,18 @@ alert(`Olá ${cliente} eu me chamo ${atendente}, em que posso ajudar?`)
    Para a variável isValueEmpty você precisa solicitar uma informação para o usuário e armazenar na variável a verificação se essa 
    informação é: null, undefined ou um texto vazio ('')*/
    
-let isFriday, isTwoGreaterThanFour, isValueEmpty = new Boolean(false)
+let isFriday, isTwoGreaterThanFour, isValueEmpty = false
 
 isFriday = confirm("Hoje é sexta-feira?")
 alert(isFriday)
-isTwoGreaterThanFour = confirm ("dois é maior que quatro?")
-alert(isTwoGreaterThanFour)
+isTwoGreaterThanFour = 2 > 4
+alert("dois é maior que quatro: " + isTwoGreaterThanFour)
 
 let info = prompt("informe a informação")
 
-if (info === null || info === undefined || info === '' ) {
-    isValueEmpty = true
-}
+isValueEmpty = (info === null || info === undefined || info === '' )
 
-alert(isValueEmpty)
+alert("é um valor vazio? " + isValueEmpty)
 
 
 /* 4) Crie um programa que recebe dois números e uma operação desejada e mostre o resultado dessa operação 
@@ -53,59 +51,58 @@ alert(isValueEmpty)
     Caso a validação encontre um erro mostre um alerta informando o usuário;
     Obs: Lembrando também que não é possível dividir por zero (ou seja, tratem isso também);*/
 
-let firstValue = prompt ("Informe o número ")
-let secondValue = prompt ("Informe outro número ")
-if (isNaN(firstValue) || isNaN(secondValue)) {
-    alert("Informe valores válidos")    
-} 
+// let firstValue = prompt ("Informe o número ")
+// let secondValue = prompt ("Informe outro número ")
 
-let operation = prompt ("Qual operação?")
-
-if (secondValue === '0' && operation==='/') {
-    alert('não é possível dividir por zero!')
-} else {
-    switch (operation.trim()) {
-        case '+':
-            alert(`Resultado: ${parseInt(firstValue) + parseInt(secondValue)}`)
-            break
-        case '-':
-            alert(`Resultado: ${parseInt(firstValue) - parseInt(secondValue)}`)
-            break
-        case '/':
-            alert(`Resultado: ${parseInt(firstValue) / parseInt(secondValue)}`)
-            break
-        case '*':
-            alert(`Resultado: ${parseInt(firstValue) * parseInt(secondValue)}`)
-            break
-        default:
-            alert("Erro. Contate administrador.")
-    }
-}
-
-
+// if (isNaN(firstValue) || isNaN(secondValue)) {
+//     alert("Informe valores válidos")    
+// } else {
+//     let operation = prompt ("Qual operação?")
+//         switch (operation.trim()) {
+//             case '+':
+//                 alert(`Resultado: ${parseInt(firstValue) + parseInt(secondValue)}`)
+//                 break
+//             case '-':
+//                 alert(`Resultado: ${parseInt(firstValue) - parseInt(secondValue)}`)
+//                 break
+//             case '/':
+//                 if (secondValue==='0'){
+//                     alert('não é possível dividir por zero!')
+//                 } 
+//                 else{
+//                     alert(`Resultado: ${parseInt(firstValue) / parseInt(secondValue)}`)
+//                 }
+//                 break
+//             case '*':
+//                 alert(`Resultado: ${parseInt(firstValue) * parseInt(secondValue)}`)
+//                 break
+//             default:
+//                 alert("Erro. Contate administrador.")
+//         }
+//     }
 
 /*5) Crie um programa que recebe 3 notas (T1, T2 e P1) como números reais (float, ex: 5.2), realize a média dessas notas e 
      realize a seguinte verificação:
         (menor que 5 = alerta de reprovado, de 5 até 7 = alerta de recuperação e acima de 7 = alerta de parabenização)
         Obs: adicione as validações para ter certeza que os números inseridos estão certos;*/
 
-let n1 = prompt("Informe primeira nota")
-let n2 = prompt("Informe segunda nota")
-let n3 = prompt("Informe terceira nota")
-let mediaTotal;
+// let n1 = prompt("Informe primeira nota")
+// let n2 = prompt("Informe segunda nota")
+// let n3 = prompt("Informe terceira nota")
+// let mediaTotal;
 
-if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
-    alert("Informe valores válidos")    
-} else {
-    mediaTotal = ((parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3)
-    if (mediaTotal < 5) {
-        alert("Reprovado")
-    } else if (mediaTotal >=5 && mediaTotal <=7){
-        alert("Recuperação")
-    } else {
-        alert("Aprovado. Parabéns")
-    }
-}
+// if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
+//     alert("Informe valores válidos")    
+// } else {
+//     mediaTotal = ((parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3)
+//     if (mediaTotal < 5) {
+//         alert("Reprovado")
+//     } else if (mediaTotal >=5 && mediaTotal <=7){
+//         alert("Recuperação")
+//     } else {
+//         alert("Aprovado. Parabéns")
+//     }
+// }
 
 
 /* 6) Crie um programa que solicite para o usuário que escolha uma das opções do menu:
@@ -116,30 +113,29 @@ if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
     4 - Sair (exibe alerta de "Ok")
     Obs: caso não seja nenhuma dessas opções exiba um alerta de opção inválida*/
 
-let opcao = prompt("Digite o que deseja \n\n 1- Checkin \n 2- Checkout \n 3- Estender \n 4- Sair\n")
-let confirmacaoCheckout, quantidadeEstadia
+// let opcao = prompt("Digite o que deseja \n\n 1- Checkin \n 2- Checkout \n 3- Estender \n 4- Sair\n")
 
-switch (opcao) {
-    case '1':
-        alert("Seja bem vindo(a)")
-        break
-    case '2':
-        confirmacaoCheckout = confirm("Deseja prosseguir com o checkout?")
-        if (confirmacaoCheckout === true) {
-            alert("Que pena!")
-        }
-        break
-    case '3':
-        quantidadeEstadia = prompt("Quantos dias gostaria de estender?")
-        if (!isNaN(quantidadeEstadia)) {
-            alert("Sua reserva foi atualizada com sucesso!")
-        } else {
-            alert("Informe quantidade válida")
-        }
-        break
-    case '4':
-        alert("Ok")
-        break
-    default:
-        alert("Informe opção válida")
-}
+// switch (opcao) {
+//     case '1':
+//         alert("Seja bem vindo(a)")
+//         break
+//     case '2':
+//         let confirmacaoCheckout = confirm("Deseja prosseguir com o checkout?")
+//         if (confirmacaoCheckout) {
+//             alert("Que pena!")
+//         }
+//         break
+//     case '3':
+//         let quantidadeEstadia = prompt("Quantos dias gostaria de estender?")
+//         if (!isNaN(quantidadeEstadia)) {
+//             alert("Sua reserva foi atualizada com sucesso!")
+//         } else {
+//             alert("Informe quantidade válida")
+//         }
+//         break
+//     case '4':
+//         alert("Ok")
+//         break
+//     default:
+//         alert("Informe opção válida")
+// }
